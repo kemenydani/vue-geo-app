aabf5aabadbdf978db91b8a8017d38c0c00f6eeb
kemenydani <kemenydani93@gmail.com>
2018-01-11 16:47:39 +0100

    removed vue-draggable

---
+++ src/components/interface/layerswitcher/layerswitcher.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>
+	
+	</div>
+</template>
+
+<script>
+	
+	export default {
+		name: 'new-component',
+		data() {
+			return {
+				data_field: 'data_field'
+			}
+		},
+		mounted() {
+			console.log('new component mounted');
+		}
+	}
+</script>
+
+<style>
+
+</style>
